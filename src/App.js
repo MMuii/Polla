125d3352cc23f78bddff5367c65ed97382e71723
mmui <muimuikrol@gmail.com>
2020-02-17 02:03:19 +0100

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Router, Switch, Route, Redirect } from 'react-router-dom';
 
-import Error404 from './components/Error404';
 import CreatePoll from './components/CreatePoll';
 import AnonymousPoll from './components/anonymousPoll/AnonymousPoll';
 
@@ -12,11 +11,11 @@ function App() {
         <Router history={history}>
             <div className="App">
                 <Switch>
-                    <Redirect exact from="/" to="/create-poll" /> 
                     <Route exact path="/create-poll" component={CreatePoll} />
                     <Route exact path="/poll/:pollId" component={AnonymousPoll} />
                     <Route exact path="/poll/:pollId/results" component={AnonymousPoll} />
-                    <Route path="/" component={() => <Error404 message="There is no URL you're looking for."/>} />
+                    <Redirect from="/" to="/create-poll" /> 
+
                 </Switch>
             </div>
         </Router>
